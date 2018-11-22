38ec28b6e5e90ba296bb0389793c2cb4201a0310
Rudy Macias <rudymacias@me.com>
2018-11-22 05:59:24 -0600

    Added ComicBookRespository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,22 +8,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Id = 1, Name = "Dan Slott", Role = "Script" },
-                    new Artist() { Id = 2, Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artist() { Id = 3, Name = "Victor Olazaba", Role = "Inks" },
-                    new Artist() { Id = 4, Name = "Edgar Delgado", Role = "Colors" },
-                    new Artist() { Id = 5, Name = "Chris Eliopoulos", Role = "Letters" }
-                }
-            };
-
-            return View( comicBook );
+            return View();
         }
     }
 }
