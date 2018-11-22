86a4a7358f1bf9dd307429e230f5360a49c105fd
Rudy Macias <rudymacias@me.com>
2018-11-21 22:55:03 -0600

    Added Comic Book and Artist models

---
+++ src/ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
